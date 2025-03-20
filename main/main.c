108136a635418a6d035233d869cf9bc6328b692b
marcelocp3 <marcelocp3@al.insper.edu.br>
2025-03-20 10:47:41 -0300

    teste2

--- main/main.c
+++ main/main.c
@@ -3,10 +3,14 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
+#include "hardware/rtc.h"
+#include "pico/util/datetime.h"
+#include <string.h>
 
-#define TRIGGER_PIN   15
-#define ECHO_PIN      14
+#define TRIGGER_PIN   14
+#define ECHO_PIN      15
 #define SENSOR_TIMEOUT_US 30000
+#define ALARM_INTERVAL_MS 1000
 
 volatile bool measurement_in_progress = false;
 volatile bool new_measurement_available = false;
@@ -14,6 +18,10 @@ volatile bool sensor_failure = false;
 volatile uint32_t pulse_width_us = 0;
 absolute_time_t echo_rise_time;
 alarm_id_t sensor_alarm_id;
+alarm_id_t general_alarm_id;
+volatile bool timer_fired = false;
+volatile int last_distance_cm = -1;
+volatile absolute_time_t last_measurement_time;
 
 int64_t sensor_timeout_callback(alarm_id_t id, void *user_data) {
     if (measurement_in_progress) {
@@ -23,7 +31,6 @@ int64_t sensor_timeout_callback(alarm_id_t id, void *user_data) {
     }
     return 0;
 }
-
 
 void echo_gpio_callback(uint gpio, uint32_t events) {
     if (gpio_get(ECHO_PIN)) { 
@@ -42,15 +49,35 @@ void echo_gpio_callback(uint gpio, uint32_t events) {
     }
 }
 
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    timer_fired = true;
+    return 0; // Não reagendar
+}
+
+
 int main(void) {
     stdio_init_all();
     gpio_init(TRIGGER_PIN);
     gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
     gpio_put(TRIGGER_PIN, 0);
+    alarm_id_t alarm = add_alarm_in_ms(300, alarm_callback, NULL, false);
 
     gpio_init(ECHO_PIN);
     gpio_set_dir(ECHO_PIN, GPIO_IN);
     gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &echo_gpio_callback);
+
+    datetime_t t = {
+        .year  = 2025,
+        .month = 03,
+        .day   = 19,
+        .dotw  = 3,
+        .hour  = 11,
+        .min   = 30,
+        .sec   = 00
+    };
+    
+    rtc_init();
+    rtc_set_datetime(&t);
 
     while (1) {
 
@@ -65,15 +92,29 @@ int main(void) {
         }
 
         if (sensor_failure) {
-            printf("Falha\n");
+            printf("Falha no sensor\n");
         } else {
             int distance_cm = pulse_width_us / 58;
-            printf("%d cm\n", distance_cm);
+            printf("%d/%d/%d %d:%d %dcm\n", t.day, t.month, t.year, t.hour, t.min, distance_cm);
+
+            absolute_time_t now = get_absolute_time();
+            if (distance_cm == last_distance_cm) {
+                if (absolute_time_diff_us(last_measurement_time, now) >= ALARM_INTERVAL_MS * 1000) {
+                    general_alarm_id = add_alarm_in_ms(0, alarm_callback, NULL, false);
+                }
+            } else {
+                last_distance_cm = distance_cm;
+                last_measurement_time = now;
+            }
         }
         new_measurement_available = false;
 
+        if (timer_fired) {
+            timer_fired = false;
+            printf("Alarme acionado!\n");
+        }
+
         sleep_ms(1000);
     }
-
     return 0;
 }
